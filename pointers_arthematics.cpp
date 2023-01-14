7528202e964b76a8821b7975ec472a0dc33d7993
Siddhant <sidbnjadhav@gmail.com>
2023-01-14 19:35:07 +0530

    Pointers_arthematics

--- pointers_arthematics.cpp
+++ pointers_arthematics.cpp
@@ -7,7 +7,8 @@ int main(){
     int* p=&i;
 
     cout<<p<<endl;
-    p++; //It does not add one bit to current address, but it points to next adress storage i.e skips 4 bytes.
+    //It does not add one bit to current address, but it points to next adress storage i.e skips 4 bytes.
+    p++;
     cout<<p<<endl;
     p++;
     cout<<p<<endl;
