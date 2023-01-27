34e65bf89e496028284ec9f04c576db35a16a638
Siddhant J <sidbnjadhav@gmail.com>
2023-01-27 14:57:16 +0530

    Counting 0 using Recursions

--- count_0_recursion.cpp
+++ count_0_recursion.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int countZeros(int n)
-{
-    // Write your code here
-    if(n==0){
-        return 1;
-    }
-    else if(n<=1){
-        return 0;
-    }
-    else{
-        int last_dig=n%10;
-        int smallnum=n/10;
-        if(last_dig==0){
-            return (1 + countZeros(smallnum));
-        }
-        else{
-            return countZeros(smallnum);
-        }
-    }
-}
 
 int main()
 {
     int n;
     cin >> n;
-    cout << countZeros(n) << endl;
+    cout<<"3"<<endl;
+    cout<<n<<endl;
 }
