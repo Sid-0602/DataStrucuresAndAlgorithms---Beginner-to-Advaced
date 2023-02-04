d22b580d5856fe3f2ad9599e6a653b494c1f4df9
Siddhant J <sidbnjadhav@gmail.com>
2023-02-04 20:25:16 +0530

    Merge sort with Comments

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 void merge(int input[], int l, int mid, int r){
-
+    //creating size of a and b arrays: 
     int n1 = mid-l+1;
     int n2= r-mid;
     //Constructing 2 temporary arrays: 
     int a[n1];
     int b[n2];
 
+    //filling a and b array with the elements from start -> mid and mid+1 -> end:
     for(int i=0; i<n1;i++){
         a[i] = input[l+i];
     }
@@ -18,10 +19,11 @@ void merge(int input[], int l, int mid, int r){
         b[i] = input[mid+1+i];
     }
 
+    //initize the varaibles i,j and k for using them as pointers:
     int i=0;
     int j=0;
     int k=l;
-
+    //comparing a[i] and b[j] and then filling them into input[k]:
     while(i<n1 && j<n2){
         if(a[i]<b[j]){
             input[k] = a[i];
@@ -35,12 +37,13 @@ void merge(int input[], int l, int mid, int r){
         }
     }
 
+    //if j pointer runs to end before i, then this loop fills value of remaining elements in a array in sorted order.
     while(i<n1){
         input[k] = a[i];
             k++;
             i++;
     }
-
+    //if i pointer runs to end before j, then this loop fills value of remaining elements in b array in sorted order.
     while(j<n2){
         input[k] = b[j];
             k++;
@@ -53,10 +56,13 @@ void mergeSort1(int input[], int l, int r){
 
     if(l<r){
         int mid= (l+r)/2;
+
+        //calling mergesort function recursively for first half of array.
         mergeSort1(input,l,mid);
-        
+        //calling mergesort function recursively for last half of array.
         mergeSort1(input,mid+1,r);
 
+        //calling merge function.
         merge(input, l, mid, r);
         
     }
