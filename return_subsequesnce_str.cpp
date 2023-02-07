b7a6e6ec72b607dd8eae4e5fbaad7bccedc35a35
Siddhant J <sidbnjadhav@gmail.com>
2023-02-07 21:55:56 +0530

    subsequence.update()

--- return_subsequesnce_str.cpp
+++ return_subsequesnce_str.cpp
@@ -2,18 +2,20 @@
 #include <cstring>
 using namespace std;
 
+//int this function and are returning size of output array and filling the array also.
 int subs(string input, string output[]){
-    if(input.empty()){  //this checks if string is empty: 
-        output[0] = "[ ]";
+    if(input.empty()){  //this checks if string is empty 
+        output[0] = "[]";
         return 1;
     }
     
-    string smallstring = input.substr(1);
-    int smalloutputSize = subs(smallstring,output); //this fills output with all other combinations without first substring:
+    string smallstring = input.substr(1); //it gives us string from next index, i.e from index 1 and not from index 0.
+    cout<<"The reduced substring is: "<< smallstring<<endl;
+    int smalloutputSize = subs(smallstring,output); //this fills output with all other combinations without first substring.  
     for(int i =0 ; i<smalloutputSize;i++){
         output[i + smalloutputSize] = input[0] + output[i];
     }
-    return 2 * smalloutputSize;
+    return (2 * smalloutputSize);
 
 }
 
@@ -21,7 +23,7 @@ int main(){
     string input;
     cin>>input;
     string * output = new string[1000];
-    int count = subs(input, output);
+    int count = subs(input, output); //returns count of all possible subsequences in output array.
     for(int i=0;i<count;i++){
         cout<<output[i]<<endl;
     }
