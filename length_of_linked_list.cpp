714134478502420afec8b0d1e22dbe8dc6144f90
Siddhant J <sidbnjadhav@gmail.com>
2023-02-24 12:45:24 +0530

    Calculating length of Linked-list.

---
+++ length_of_linked_list.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+
+int length(Node *head)
+{
+    //Write your code here
+    int counter = 0;
+    Node *temp = head;
+
+    while(temp!=NULL){
+        counter++;
+        temp=temp->next;
+    }
+
+    return counter;
+    
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}
