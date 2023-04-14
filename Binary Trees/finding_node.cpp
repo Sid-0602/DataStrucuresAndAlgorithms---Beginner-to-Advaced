37adaa14ce16090e132a473c1636ff77a49aeee4
Siddhant J <sidbnjadhav@gmail.com>
2023-04-14 20:19:25 +0530

    Finding Nodes in BT

---
+++ Binary Trees/finding_node.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+bool flag=false;
+
+bool isNodePresent(BinaryTreeNode<int> *root, int x) {
+    // Write your code here
+
+    if(root==NULL){
+        return false;
+    }
+
+    if(root->data==x){
+        flag=true;
+        return flag;
+    }
+
+    isNodePresent(root->left,x);
+    isNodePresent(root->right,x);
+
+    return flag; 
+}
+
+
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int x;
+    cin >> x;
+    cout << ((isNodePresent(root, x)) ? "true" : "false");
+}
