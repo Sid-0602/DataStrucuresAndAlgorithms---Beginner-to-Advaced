d009956e8901c7bbae9b3f346246b6ef7116f667
Siddhant J <sidbnjadhav@gmail.com>
2023-06-20 00:01:06 +0530

    Searching in Sorted array

--- Basics of C++/Search_rotated_array.cpp
+++ Basics of C++/Search_rotated_array.cpp
@@ -3,10 +3,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-    int search(vector<int>& nums, int target) {
-        int search(vector<int>& nums, int target) {
-        int start=0, end=nums.size()-1;
-        while(start<=end){
+int search(vector<int>& nums, int target) {
+    int start=0, end=nums.size()-1;
+    while(start<=end){
             int mid=start + (end-start)/2;
             if(nums[mid] == target) return mid;
             else if(nums[mid]>=nums[start]){
@@ -19,8 +18,8 @@ using namespace std;
             }
         } 
         return -1;  
-	}
 }
+
 
 int main(){
 	vector<int> nums = {2,3,1};
