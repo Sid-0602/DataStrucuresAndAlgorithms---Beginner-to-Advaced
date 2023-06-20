501631b517ff174c8ef7b6350128138dc0525ef3
Siddhant J <sidbnjadhav@gmail.com>
2023-06-20 17:32:02 +0530

    update push

---
+++ BST/xyz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+    int search(vector<int>& nums, int target) {
+    int start=0, end=nums.size()-1;
+    while(start<=end){
+            int mid=start + (end-start)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid]>=nums[start]){
+                if(target>=nums[start] && target<nums[mid]) end = mid-1;
+                else start=mid+1;
+            }
+            else{
+                if(target>nums[mid] && target <=nums[end]) start = mid+1;
+                else end = mid-1;
+            }
+        } 
+        return -1;  
+	}
+
+
+int main(){
+	vector<int> nums = {2,3,1};
+	cout<<search(nums,3);
+}
--- Stacks/check_reduanant_brackets.cpp
+++ Stacks/check_reduanant_brackets.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 bool checkRedundantBrackets(string expression) {
 	// Write your code here
+
+
+    
 }
 
 int main() {
