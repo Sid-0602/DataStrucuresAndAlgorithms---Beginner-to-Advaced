747e8761826c78bcddd9a5d0fa7db5d1488dda88
Siddhant J <sidbnjadhav@gmail.com>
2023-06-22 17:54:42 +0530

    removeMin class Push

--- Priority Queue/Priority_queue.h
+++ Priority Queue/Priority_queue.h
@@ -13,14 +13,7 @@ class PriorityQueue{
 
     //public function: 
 
-    int removeMin() { 
-        // Write your code here
-        int ans = pq[0];
-        pq[0] = pq[pq.size()-1]; //assigning the value of
-
-        pq.pop_back(); //removing the last element. 
-
-    }
+    
 
     void insert(int element){
         //insert element into queue but we need to preserve heap-order property:
@@ -56,5 +49,54 @@ class PriorityQueue{
     bool isEmpty(){
         //checks the size of pq
         return pq.size()==0;
+    }
+
+    int removeMin() { 
+        // Write your code here
+
+        if(isEmpty){
+            return 0; //priority queue is empty. 
+        }
+        int ans = pq[0]; //store the min element in ans variable.
+        pq[0] = pq[pq.size()-1]; //assigning the value of
+        pq.pop_back(); //removing the last element. 
+
+        
+        //Down-Heapify: 
+        int parent_index = 0 ;
+        int left_child=2*(parent_index)+1;
+        int right_child=2*(parent_index)+2;
+
+        
+        while(left_child<pq.size()){
+            
+            int min_index = parent_index; //intialising min index with parent index.
+            if(pq[min_index]>pq[left_child]){ 
+                min_index = left_child;
+            } //if left is smaller, store it's index in min_index.
+
+            if(right_child<pq.size() && pq[right_child]<pq[min_index])
+            { 
+                min_index = right_child;
+            } //if right is smaller, store it's index in min_index.
+
+
+            if(parent_index == min_index){
+                break;
+            }
+            //swap the parent index with
+            int temp = pq[min_index];
+            pq[min_index] = pq[parent_index];
+            pq[parent_index] = temp;
+
+
+            parent_index = min_index; 
+            int left_child=2*(parent_index)+1;
+            int right_child=2*(parent_index)+2;
+
+        }
+
+        return ans;
+
     }
 };
