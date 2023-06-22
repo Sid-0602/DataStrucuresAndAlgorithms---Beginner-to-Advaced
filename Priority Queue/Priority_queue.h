ce716cd8ea108e99676af24925b7b3151e467aa9
Siddhant J <sidbnjadhav@gmail.com>
2023-06-22 16:00:43 +0530

    Update push

--- Priority Queue/Priority_queue.h
+++ Priority Queue/Priority_queue.h
@@ -18,31 +18,12 @@ class PriorityQueue{
         // Write your code here
         int ans = pq[0];
 
-
+   ;
     }
 
     void insert(int element){
         //insert element into queue but we need to preserve heap-order property:
-        pq.push_back(element);
-
-        int childIndex = pq.size()-1; //this is childIndex which we need to upheap.
-
-        while(childIndex>0){
-            int parentIndex  = (childIndex-1)/2;
-
-            if(pq[childIndex]<pq[parentIndex]){
-
-                //swap it with parent data node.
-                int temp = pq[childIndex];
-                pq[childIndex] = pq[parentIndex];
-                pq[parentIndex]=temp; 
-            }
-            else{
-                break;
-            }
-
-            childIndex = parentIndex; //update childIndex with ParentIndex after swapping.
-        }
+        
     
     }
 
