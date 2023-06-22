aaed94a9c3e240978feb9fc94d42b16f14f32d52
Siddhant J <sidbnjadhav@gmail.com>
2023-06-22 16:57:58 +0530

    Priority Queue Push

--- Priority Queue/Priority_queue.h
+++ Priority Queue/Priority_queue.h
@@ -8,22 +8,38 @@ class PriorityQueue{
     public :
 
     //Constructor: 
-
     PriorityQueue(){}
 
 
     //public function: 
 
-     int removeMin() {
+    int removeMin() { 
         // Write your code here
         int ans = pq[0];
+        pq[0] = pq[pq.size()-1]; //assigning the value of
 
-   ;
+        pq.pop_back(pq.size()-1); //removing the last element. 
+
     }
 
     void insert(int element){
         //insert element into queue but we need to preserve heap-order property:
-        
+        pq.push_back(element);
+
+        int childIndex = pq.size()-1;
+
+        while(childIndex>0){
+            int parentIndex = (childIndex-1)/2;
+            if(pq[childIndex]<pq[parentIndex]){
+                //swap child and parent:
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp; 
+            }
+            else{break;}    //break the loop when the above condition is not valid, that means, element reached correct position    
+
+            childIndex = parentIndex; //assign the parentIndex at childIndex
+        }
     
     }
 
