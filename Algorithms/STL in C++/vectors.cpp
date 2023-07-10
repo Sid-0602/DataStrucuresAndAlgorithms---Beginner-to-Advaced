a868b2f75f1e3fa73e6c0928700446538fddb2d4
Siddhant J <sidbnjadhav@gmail.com>
2023-07-10 23:14:16 +0530

    iterator, auto assign and for-each loop

--- Algorithms/STL in C++/vectors.cpp
+++ Algorithms/STL in C++/vectors.cpp
@@ -12,13 +12,61 @@ int main(){
 
     v.push_back(1);
     v.emplace_back(2); //this does same work as that of push_back but it is much faster than push_back.
+    v.push_back(3);
+    v.push_back(4);
 
     vector<pair<int,int>>vec; //this will create a vector with data type as pair.
     vec.push_back({1,2});
     vec.emplace_back(1,2); //emplace will know that this is pair value and it will push_back it into the array. 
-    
 
-    //concept of itration in STL:
+
+
+    //concept of iteration in STL:
+    
+    vector<int> :: iterator it = v.begin(); //iterator will point to the start of vector memory address.
+    cout<<"ITERATOR BEGIN"<<endl;
+    cout<<*(it)<<endl;
+    it++; //this shift the pointer to next element address.
+    cout<<*it<<endl;
+
+    cout<<"ITERATOR END"<<endl;
+    vector<int> :: iterator it_e = v.end(); //iterator will point to the end of vector memory address.
+    cout<<"This gives next memory location of last element (hence, garbage) : "<<*it_e<<endl;
+    it_e--;
+    cout<<*it_e<<endl;
+
+    /*
+
+    This is used to treat the vector in reverse manner and then point the rend to start and rbegin to end of vector.
+    cout<<"ITERATOR REND AND RBEGIN"<<endl;
+    vector<int>::iterator it_re = v.rend();
+    vector<int>::iterator it_rbg = v.rbegin();
+
+    */
+
+    // looping vector using iterator: 
+
+    for(vector<int>::iterator it = v.begin();it!=v.end();it++){
+        cout<<"Looping using iterator: "<<*it<<endl;
+    }
+    
+    //the same code can be written using "auto" operator: 
+
+    for(auto it = v.begin();it!=v.end();it++){
+        cout<<"Looping using auto assign operator "<<*it<<endl;
+    } 
+
+    //Using for-each Loop: 
+
+    cout<<"FOREACH LOOP"<<endl;
+    for(auto it:v){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+
+
+
+
 
 
 
