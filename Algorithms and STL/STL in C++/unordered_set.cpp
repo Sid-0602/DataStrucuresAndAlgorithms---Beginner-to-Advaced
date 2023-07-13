91ae021e8aeb6c7faac61c536c203bd01a945084
Siddhant J <sidbnjadhav@gmail.com>
2023-07-13 11:00:54 +0530

    Unordered set

---
+++ Algorithms and STL/STL in C++/unordered_set.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unordered_set<int> st; //syntax 
+
+    //this is used to insert elements into the set.
+    st.insert(1);
+    st.insert(1);
+    st.insert(2);
+    st.insert(2);
+    st.insert(3);
+
+    //to erase/remove a specific entry: 
+    st.erase(3);
+   
+}
