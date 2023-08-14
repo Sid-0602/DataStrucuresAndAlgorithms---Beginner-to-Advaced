38537af2afdb41ee498c8ea1c7af6127f6bcb6de
Siddhant J <sidbnjadhav@gmail.com>
2023-08-14 22:11:03 +0530

    minimum size subarray sum using Sliding Window

---
+++ Algorithms and STL/Algorithms/Sliding Window Algo/minimum_size_subarray_sum.cpp
@@ -0,0 +1,16 @@
+/**
+    Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+    subarray
+    whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+
+    Example 1:
+
+    Input: target = 7, nums = [2,3,1,2,4,3]
+    Output: 2
+    Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
